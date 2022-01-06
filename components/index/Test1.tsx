c80b51fadf1927c0ece072817c5f5233a97bafd7
Ryan Bender <52826517+ryanbender2@users.noreply.github.com>
2022-01-06 11:39:28 -0500

    added creation of opening animation

--- components/index/Test1.tsx
+++ components/index/Test1.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import styles from '../../styles/Home.module.css'
 
 export default function Test1() {
     return (
-        <motion.div className="">
-            
+        <motion.div className={styles.title}>
+            Hello there
         </motion.div>
     )
 }
